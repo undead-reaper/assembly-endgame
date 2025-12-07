d4206d67875e4150f4616267c17aa888632b36e9
Dharam Soni <dharamsoni1010@gmail.com>
2025-12-07 14:53:37 +0530

    Migrated to NextJS 16 and updated deps

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactCompiler: true,
   experimental: {
-    reactCompiler: true,
+    useLightningcss: true,
   },
 };
 
